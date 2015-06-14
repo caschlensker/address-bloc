d65a9746c4625abfb729b5c41660f39772cf9b66
Catherine Schlensker <catherine.ann.park@gmail.com>
2015-06-14 12:31:08 -0700

    stub

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,11 +1,12 @@
 require_relative "entry.rb"
+require "csv"
 
 class AddressBook
   attr_accessor :entries
 
   def initialize
-   @entries = []
-  end   
+    @entries = []
+  end
 
   def add_entry(name, phone, email)
 
@@ -13,15 +14,27 @@ class AddressBook
     @entries.each do |entry|
       if name < entry.name
         break
-      end 
+      end
 
       index += 1
-    end  
+    end
 
     @entries.insert(index, Entry.new(name, phone, email))
-  end   
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+
+    return csv.count
+  end
 
   def remove_entry(entry)
     entries.delete(entry)
-  end             
-end 
+  end
+end
